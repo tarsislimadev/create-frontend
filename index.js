e0408b9884f4e17f0063e46680a7e501fbca9c77
Tarsis <br.tmvdl@gmail.com>
2023-12-13 18:09:54 -0300

    Updating app files

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+#!/usr/local/bin/node
+
 console.log(require('./package.json').name)
+const path = require('path')
+const fs = require('fs')
+
+const [, app_path = 'app'] = process.argv
+fs.mkdirSync(path.resolve(__dirname, app_path))
+
+const project_path = path.resolve('.', 'project')
+fs.readdirSync()
+  .map((name) => {
+    const project_file = path.resolve(project_path, name)
+    const app_file = path.resolve(app_path, name)
+
+    const project_stream = fs.createReadStream(project_file) 
+    const app_stream = fs.createWriteStream(app_file) 
+    project_stream.pipe(app_stream)
+  })
